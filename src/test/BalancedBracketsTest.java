416c792960da10438b2e4c812e97129164b21793
James Ginns <starvagrant@yahoo.com>
2020-01-22 19:01:37 -0600

    Added Twelve Test Cases, 5 failures

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,62 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void testEmptyString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
     }
 
+    @Test
+    public void testSingleBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
 
+    @Test
+    public void testMatchedBracket() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testUnevenBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    public void testNestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void testSingleBracketWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[asdf"));
+    }
+
+    @Test
+    public void testBalancedBracketsWithCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[asdf]"));
+    }
+
+    @Test
+    public void testInvertedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testInvertedBracketsWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]asdf["));
+    }
+
+    @Test
+    public void testNestedUnmatchedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]["));
+    }
+
+    @Test
+    public void testNestedUnmatchedBracketsWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("as]asd[asd]asd["));
+    }
+
+    @Test
+    public void testNestedandUnnestedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
 }
